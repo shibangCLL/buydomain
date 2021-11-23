ce0eaf778477473e5a34f7e935490cb719713f0a
chenglulu <sb_cll@163.com>
2021-11-23 09:20:58 +0800

    first

--- domain/models.py
+++ domain/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # from django.contrib.auth.models import User
 from member.models import Member
+from registerplatform.models import RegisterPlatform
 
 
 class DomainType(models.Model):
@@ -10,19 +11,11 @@ class DomainType(models.Model):
         return self.type
 
 
-class RegisterPlatform(models.Model):
-    platform = models.CharField('注册平台', max_length=150)
-
-    def __str__(self):
-        return self.platform
-
-
 class Domain(models.Model):
     domain = models.CharField('域名', max_length=255)
     add_date = models.DateTimeField(verbose_name='添加时间', auto_now_add=True)
     expired_date = models.DateTimeField(verbose_name='域名释放时间', blank=True, null=True)
     register_date = models.DateTimeField(verbose_name='域名注册时间', blank=True, null=True)
-
     domain_type = models.ForeignKey(DomainType, verbose_name='域名后缀', on_delete=models.PROTECT)
     register_platform = models.ManyToManyField(RegisterPlatform, verbose_name='注册平台')
     user = models.ManyToManyField(Member, verbose_name='域名归属')
---
+++ registerplatform/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegisterplatformConfig(AppConfig):
+    name = 'registerplatform'
