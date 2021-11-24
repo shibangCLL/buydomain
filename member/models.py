5d40c59292be0eb6e0531a1745917bd2ef20e655
chenglulu <sb_cll@163.com>
2021-11-24 16:27:27 +0800

    register ok

--- member/models.py
+++ member/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Member(User):
     """自定义会员模型类"""
-    mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
+    mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号', blank=True, null=True)
 
     class Meta:
         verbose_name = '会员'
--- member/views.py
+++ member/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import View
+from .models import Member
 
 
 # Create your views here.
@@ -7,7 +8,24 @@ class RegisterView(View):
     """用户注册"""
 
     def get(self, request):
-        return render(request, 'registration.html')
+        return render(request, 'register.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        r_password = request.POST.get('r_password')
+        if password != r_password:
+            return HttpResponse('两次输入的密码不同！')
+        else:
+            same_name_user = Member.objects.filter(username=username)
+            if same_name_user:
+                return HttpResponse('用户名已经存在！')
+            same_email_user = Member.objects.filter(email=email)
+            if same_email_user:
+                return HttpResponse('该邮箱已经被注册了！')
+        new_member = Member.objects.create_user(username=username, email=email, password=password)
+        return HttpResponse('ok')
 
 
 class LoginView(View):
@@ -15,4 +33,3 @@ class LoginView(View):
 
     def get(self, request):
         return render(request, 'login.html')
-
